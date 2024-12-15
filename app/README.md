9d2a1d2f2484f7b3911245c09970233a1d4e00fb
AngelaHarney <AngelaMarieHarney@gmail.com>
2024-12-15 11:24:45 -0800

    snowflake-games-readme

--- app/README.md
+++ app/README.md
@@ -1,3 +1,17 @@
 # Snowflake Native App - Add
 
-This is a sample Snowflake Native App that adds two numbers using Streamlit and Python.
+Learn Snowflake while you answer game questions.  You select SQL syntax as answers to gain points.  You build a script along the way that is presented at the end of the game for you to copy and paste into a SQL Worksheet to try it out.
+
+Attending the local Snowflake User Groups where these games are played with a group of people have the potential to provide the opportunity to win Snowflake swag as prizes, including the opportunity to possibly with the cherished SnowBear.
+
+Some of the more intermediate or advanced games provide challenges that are scored after discussion with an event coordinator and to provide real-world learning of similar situations.
+
+Create your own game and contribute to the application through a local user group!
+
+
+
+
+Note:  Some of the drop down lists cover the status of your select, so you may have to click off the drop down list in order to see your status and points.
+
+
+Snowflake Tools
--- app/setup_script.sql
+++ app/setup_script.sql
@@ -5,31 +5,6 @@
 -- 1. Create application roles
 CREATE APPLICATION ROLE IF NOT EXISTS app_public;
 
--- 2. Create a versioned schema to hold those UDFs/Stored Procedures
-CREATE OR ALTER VERSIONED SCHEMA core;
-GRANT USAGE ON SCHEMA core TO APPLICATION ROLE app_public;
-
--- 3. Create UDFs and Stored Procedures using the python code you wrote in src/module-add, as shown below.
-CREATE OR REPLACE FUNCTION core.add(x NUMBER, y NUMBER)
-  RETURNS NUMBER
-  LANGUAGE PYTHON
-  RUNTIME_VERSION=3.8
-  PACKAGES=('snowflake-snowpark-python')
-  IMPORTS=('/module-add/add.py')
-  HANDLER='add.add_fn';
-
-CREATE OR REPLACE PROCEDURE core.increment_by_one(x NUMBER)
-  RETURNS NUMBER
-  LANGUAGE PYTHON
-  RUNTIME_VERSION=3.8
-  PACKAGES=('snowflake-snowpark-python')
-  IMPORTS=('/module-add/add.py')
-  HANDLER='add.increment_by_one_fn';
-
--- 4. Grant appropriate privileges over these objects to your application roles.
-GRANT USAGE ON FUNCTION core.add(NUMBER, NUMBER) TO APPLICATION ROLE app_public;
-GRANT USAGE ON PROCEDURE core.increment_by_one(NUMBER) TO APPLICATION ROLE app_public;
-
 -- 5. Create a streamlit object using the code you wrote in you wrote in src/module-ui, as shown below.
 -- The `from` value is derived from the stage path described in snowflake.yml
 CREATE OR REPLACE STREAMLIT core.ui
